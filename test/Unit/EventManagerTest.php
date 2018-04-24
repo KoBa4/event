61f0cb8f7cbb80dd9d4c5af86e29c24b58bb355d
Иван <cova44ever@gmail.com>
2018-04-24 11:34:34 +0600

    Finish tests

--- test/Unit/EventManagerTest.php
+++ test/Unit/EventManagerTest.php
@@ -27,7 +27,7 @@ class EventManagerTest extends \PHPUnit_Framework_TestCase
         parent::setUp();
 
         $this->manager = new EvenManager();
-        $this->callback = function (EventInterface $event) {
+        $this->callback = function () {
             static $magic = 0;
             return ++$magic;
         };
@@ -38,11 +38,41 @@ class EventManagerTest extends \PHPUnit_Framework_TestCase
         $this->assertTrue($this->manager->attach('magic', $this->callback));
     }
 
+    public function testAttachWithPriority()
+    {
+        $this->assertTrue($this->manager->attach('magic', $this->callback, 0));
+    }
+
+    public function testAttachInvalidEvent()
+    {
+        $this->assertFalse($this->manager->attach(123, $this->callback));
+    }
+
+    public function testAttachInvalidCallback()
+    {
+        $this->assertFalse($this->manager->attach('magic', '54613215'));
+    }
+
+    public function testAttachInvalidPriority()
+    {
+        $this->assertFalse($this->manager->attach('magic', $this->callback, 'string'));
+    }
+
     public function testDetach()
     {
         $this->assertFalse($this->manager->detach('magic', $this->callback));
         $this->manager->attach('magic', $this->callback);
         $this->assertTrue($this->manager->attach('magic', $this->callback));
+    }
+
+    public function testDetachInvalidEvent()
+    {
+        $this->assertFalse($this->manager->detach(123, $this->callback));
+    }
+
+    public function testDetachInvalidCallback()
+    {
+        $this->assertFalse($this->manager->detach('magic', '54613215'));
     }
 
     public function testClearListeners()
@@ -60,5 +90,27 @@ class EventManagerTest extends \PHPUnit_Framework_TestCase
         $event = $this->manager->trigger('magic');
         $this->assertSame('magic', $event->getName());
         $this->assertSame(2, call_user_func($this->callback, $this->getMockBuilder(EventInterface::class)->getMock()));
+    }
+
+    public function testTriggerEventInterface()
+    {
+        $event = $this->getMockBuilder(EventInterface::class)->getMock();
+        $event->expects($this->atLeast(1))
+            ->method('getName')->willReturn('magic');
+        $this->manager->attach('magic', $this->callback);
+
+        /** @var EventInterface $event */
+        $event = $this->manager->trigger($event);
+        $this->assertSame('magic', $event->getName());
+        $this->assertSame(2, call_user_func($this->callback, $this->getMockBuilder(EventInterface::class)->getMock()));
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage Event must be EventInterface
+     */
+    public function testTriggerNotValidEvent()
+    {
+        $this->manager->trigger(123);
     }
 }
