fe7c48a3473e21c98adf2e1fec99259ddd78a372
Иван <cova44ever@gmail.com>
2018-04-24 10:54:22 +0600

    Event Manager

--- src/EvenManager.php
+++ src/EvenManager.php
@@ -8,17 +8,31 @@
 
 namespace KoBa4\Event;
 
+use KoBa4\PEvent\EventInterface;
 use KoBa4\PEvent\EventManagerInterface;
 
 class EvenManager implements EventManagerInterface
 {
+    /**
+     * @var array
+     */
+    private $events = array();
 
     /**
      * @inheritdoc
      */
     public function attach($event, $callback, $priority = 0)
     {
-        // TODO: Implement attach() method.
+        $result = false;
+        if ( is_string($event) && is_callable($callback) && is_int($priority) ) {
+            if ( !array_key_exists($event, $this->events) || !array_key_exists($priority, $this->events[$event]) ) {
+                $this->events[$event][$priority] = [];
+            }
+            array_push($this->events[$event][$priority], $callback);
+            $result = true;
+        }
+
+        return $result;
     }
 
     /**
@@ -26,7 +40,18 @@ class EvenManager implements EventManagerInterface
      */
     public function detach($event, $callback)
     {
-        // TODO: Implement detach() method.
+        $result = false;
+        if ( array_key_exists($event, $this->events) ) {
+            foreach ( $this->events[$event] as $key => $value ) {
+                if ( ($finded = array_search($callback, $value)) !== false ) {
+                    unset($this->events[$event][$key][$finded]);
+                    $result = true;
+                    break;
+                }
+            }
+        }
+
+        return $result;
     }
 
     /**
@@ -34,14 +59,33 @@ class EvenManager implements EventManagerInterface
      */
     public function clearListeners($event)
     {
-        // TODO: Implement clearListeners() method.
+        if ( array_key_exists($event, $this->events) ) {
+            unset($this->events[$event]);
+        }
     }
 
     /**
      * @inheritdoc
      */
-    public function trigger($event, $target = null, $argv = [])
+    public function trigger($event, $target = null, $argv = array())
     {
-        // TODO: Implement trigger() method.
+        if ( is_string($event) ) {
+            $event = new Event($event, $target, (array)$argv);
+        }
+
+        if ( !($event instanceof EventInterface) ) {
+            throw new \InvalidArgumentException('Event must be EventInterface');
+        }
+
+        if ( array_key_exists($event->getName(), $this->events) ) {
+            ksort($this->events[$event->getName()]);
+            foreach ( $this->events[$event->getName()] as $list ) {
+                foreach ( $list as $callable ) {
+                    call_user_func($callable, $event);
+                }
+            }
+        }
+
+        return $event;
     }
 }
--- test/Unit/EventManagerTest.php
+++ test/Unit/EventManagerTest.php
@@ -8,10 +8,57 @@
 
 namespace KoBa4\Event\Test\Unit;
 
+use KoBa4\Event\EvenManager;
+use KoBa4\PEvent\EventInterface;
+
 class EventManagerTest extends \PHPUnit_Framework_TestCase
 {
-    public function testHappy()
+    /**
+     * @var EvenManager
+     */
+    private $manager;
+    /**
+     * @var callable
+     */
+    private $callback;
+
+    public function setUp()
     {
-        $this->assertTrue(true);
+        parent::setUp();
+
+        $this->manager = new EvenManager();
+        $this->callback = function (EventInterface $event) {
+            static $magic = 0;
+            return ++$magic;
+        };
+    }
+
+    public function testAttach()
+    {
+        $this->assertTrue($this->manager->attach('magic', $this->callback));
+    }
+
+    public function testDetach()
+    {
+        $this->assertFalse($this->manager->detach('magic', $this->callback));
+        $this->manager->attach('magic', $this->callback);
+        $this->assertTrue($this->manager->attach('magic', $this->callback));
+    }
+
+    public function testClearListeners()
+    {
+        $this->manager->attach('magic', $this->callback);
+        $this->manager->clearListeners('magic');
+        $this->assertFalse($this->manager->detach('magic', $this->callback));
+    }
+
+    public function testTrigger()
+    {
+        $this->manager->attach('magic', $this->callback);
+
+        /** @var EventInterface $event */
+        $event = $this->manager->trigger('magic');
+        $this->assertSame('magic', $event->getName());
+        $this->assertSame(2, call_user_func($this->callback, $this->getMockBuilder(EventInterface::class)->getMock()));
     }
 }
